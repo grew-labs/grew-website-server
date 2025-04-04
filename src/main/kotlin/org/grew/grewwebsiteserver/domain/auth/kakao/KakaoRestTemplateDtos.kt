6ca7d6db8cf84d58d9fae3267019328315d3db7e
Yoonho Cho (Ray) <raycho521@yonsei.ac.kr>
2025-04-04 09:57:04 +0900

    feat: kakao 로그인 dto 수정

--- src/main/kotlin/org/grew/grewwebsiteserver/domain/auth/kakao/KakaoRestTemplateDtos.kt
+++ src/main/kotlin/org/grew/grewwebsiteserver/domain/auth/kakao/KakaoRestTemplateDtos.kt
@@ -3,11 +3,11 @@ package org.grew.grewwebsiteserver.domain.auth.kakao
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class KakaoTokenResponse(
+    @JsonProperty("token_type") val tokenType: String,
     @JsonProperty("access_token") val accessToken: String,
-    @JsonProperty("refresh_token") val refreshToken: String?,
     @JsonProperty("expires_in") val expiresIn: Int,
-    @JsonProperty("refresh_token_expires_in") val refreshTokenExpiresIn: Int?,
-    @JsonProperty("token_type") val tokenType: String
+    @JsonProperty("refresh_token") val refreshToken: String?,
+    @JsonProperty("refresh_token_expires_in") val refreshTokenExpiresIn: Int?
 )
 
 
