d5697f1b202593c28a0f7588a756615ae5f99349
Yoonho Cho (Ray) <raycho521@yonsei.ac.kr>
2025-04-11 17:34:45 +0900

    feat: 카카오 인증 기능 수정

--- src/main/kotlin/org/grew/grewwebsiteserver/domain/auth/kakao/KakaoAuthService.kt
+++ src/main/kotlin/org/grew/grewwebsiteserver/domain/auth/kakao/KakaoAuthService.kt
@@ -1,5 +1,9 @@
 package org.grew.grewwebsiteserver.domain.auth.kakao
 
+import org.grew.grewwebsiteserver.domain.auth.AuthResponseDto
+import org.grew.grewwebsiteserver.domain.auth.JwtService
+import org.grew.grewwebsiteserver.domain.auth.enums.AuthType
+import org.grew.grewwebsiteserver.domain.user.UserService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -15,19 +19,21 @@ class KakaoAuthService(
     @Value("\${kakao.client-id}") private val clientId: String,
     @Value("\${kakao.redirect-uri}") private val redirectUri: String,
     @Value("\${kakao.token-uri}") private val tokenUri: String,
-    @Value("\${kakao.user-info-uri}") private val userInfoUri: String
+    @Value("\${kakao.user-info-uri}") private val userInfoUri: String,
+    private val userService: UserService,
+    private val jwtService: JwtService
 ) {
-    // 임시 저장 (실제로는 DB나 Redis에 저장해야 함)
-    private var refreshTokenStore: MutableMap<Long, String> = mutableMapOf()
 
     fun kakaoLogin(code: String): AuthResponseDto {
-        val tokenResponse = getTokenByAuthCode(code)
-        val userInfo = getUserInfo(tokenResponse.accessToken)
-
-        // 새 refresh token이 있을 경우 갱신
-        tokenResponse.refreshToken?.let {
-            refreshTokenStore[userInfo.id] = it
-        }
+        println("redirectUri: $redirectUri")
+        // 1. 카카오 인가코드로 <카카오 엑세스 토큰> 받기
+        val kakaoTokenResponse = getTokenByAuthCode(code)
+        // 2. 엑세스 토큰으로 카카오에서 유저 정보 조회
+        val userInfo = getUserInfo(kakaoTokenResponse.accessToken)
+        // 3. 회원가입 또는 로그인 처리
+        val user = userService.saveOrUpdate(UserInfoDto(userInfo.id, userInfo.nickname), AuthType.KAKAO)
+        // 4. 엑세스 토큰 발급
+        val tokenResponse = jwtService.createTokenInfo(userInfo.id)
 
         return AuthResponseDto(tokenInfo = tokenResponse, nickname = userInfo.nickname)
     }
@@ -36,32 +42,32 @@ class KakaoAuthService(
         val params = mapOf(
             "grant_type" to "authorization_code",
             "client_id" to clientId,
-            "redirect_uri" to redirectUri,
             "code" to code
         )
 
         return requestToken(params)
     }
 
-    fun refreshAccessToken(userId: Long): KakaoTokenResponse {
-        val refreshToken = refreshTokenStore[userId]
-            ?: throw RuntimeException("Refresh token 없음")
-
-        val params = mapOf(
-            "grant_type" to "refresh_token",
-            "client_id" to clientId,
-            "refresh_token" to refreshToken
-        )
-
-        val tokenResponse = requestToken(params)
-
-        // 새 refresh token이 있을 경우 갱신
-        tokenResponse.refreshToken?.let {
-            refreshTokenStore[userId] = it
-        }
-
-        return tokenResponse
-    }
+    // kakao token refresh (필요시 사용)
+//    fun refreshAccessToken(userId: Long): KakaoTokenResponse {
+//        val refreshToken = refreshTokenStore[userId]
+//            ?: throw RuntimeException("Refresh token 없음")
+//
+//        val params = mapOf(
+//            "grant_type" to "refresh_token",
+//            "client_id" to clientId,
+//            "refresh_token" to refreshToken
+//        )
+//
+//        val tokenResponse = requestToken(params)
+//
+//        // 새 refresh token이 있을 경우 갱신
+//        tokenResponse.refreshToken?.let {
+//            refreshTokenStore[userId] = it
+//        }
+//
+//        return tokenResponse
+//    }
 
     private fun requestToken(params: Map<String, String>): KakaoTokenResponse {
         val headers = HttpHeaders().apply {
@@ -83,8 +89,6 @@ class KakaoAuthService(
 
         val entity = HttpEntity(null, headers)
         val response = restTemplate.exchange(userInfoUri, HttpMethod.GET, entity, KakaoUserInfo::class.java)
-        println("response: ${response.body}")
-        println("response: ${response.body?.id}")
         return response.body ?: throw RuntimeException("사용자 정보 없음")
     }
 }
