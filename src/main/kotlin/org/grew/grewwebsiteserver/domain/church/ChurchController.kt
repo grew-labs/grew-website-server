207fc493374b9a5f8489e4afe60f8512921e5b5d
Yoonho Cho (Ray) <raycho521@yonsei.ac.kr>
2025-04-25 16:39:38 +0900

    feat: 컨트롤러에서 인증권한 관리하도록 수정

--- src/main/kotlin/org/grew/grewwebsiteserver/domain/church/ChurchController.kt
+++ src/main/kotlin/org/grew/grewwebsiteserver/domain/church/ChurchController.kt
@@ -2,7 +2,10 @@ package org.grew.grewwebsiteserver.domain.church
 
 import org.grew.grewwebsiteserver.domain.auth.kakao.ApiResponse
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/churches")
