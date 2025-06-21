15b0dcaf408aac6d8e438d7bfeba462aabdf5ec3
이병찬 <woollim@toss.im>
2025-06-22 00:40:48 +0900

    게시물 조회시 카테고리를 필수 파라미터로 받도록 수정

--- src/main/kotlin/org/grew/grewwebsiteserver/domain/post/dto/PostListRequestDto.kt
+++ src/main/kotlin/org/grew/grewwebsiteserver/domain/post/dto/PostListRequestDto.kt
@@ -3,5 +3,5 @@ package org.grew.grewwebsiteserver.domain.post.dto
 import org.grew.grewwebsiteserver.domain.post.entity.PostCategory
 
 data class PostListRequestDto(
-    val category: PostCategory?
+    val category: PostCategory
 )
